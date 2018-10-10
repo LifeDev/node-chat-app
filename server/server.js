b0e414d906e96a8800d4179f9da108d65948ce68
Maksym Bliednov <maksymblyednov@gmail.com>
2018-10-10 14:27:44 -0400

    Added some sockets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath))
 //
@@ -13,7 +16,14 @@ app.use(express.static(publicPath))
 //   res.sendFile(publicPath + '/index.html')
 // });
 
+io.on('connection', (socket) => {
+  console.log('New User Connected');
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log("Client disconnected");
+  })
+});
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
