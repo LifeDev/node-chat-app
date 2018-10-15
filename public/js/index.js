6f50d71af8b855c303d28accb8f011147520a50d
Maksym Bliednov <maksymblyednov@gmail.com>
2018-10-15 17:44:49 -0400

    Autoscroll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
 
 var socket = io();
+
+function scrollToBottom () {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log("Connected to server");
 
@@ -19,6 +36,7 @@ socket.on('newMessage', function (message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 
 });
 socket.on('newLocationMessage', function(message) {
@@ -31,7 +49,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
-
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
@@ -43,6 +61,7 @@ $('#message-form').on('submit', function (e) {
       text: $('[name=message]').val()
     }, function () {
       $('[name=message]').val('');
+      $('[name=message]').focus();
     });
 
 
